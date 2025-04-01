16f0c498dccb4b451702a717e6755ab84c59dfd2
nickolashkraus <0x@nickolaskraus.io>
2025-04-01 13:08:05 -0500

    Fix errors:
      ```
      Warning: terraform "required_version" attribute is required
      Warning: Missing version constraint for provider "aws" in `required_providers`
      ```

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,17 @@
 # -----------------------------------------------------------------------------
 # AMAZON ELASTIC KUBERNETES SERVICE (EKS)
 # -----------------------------------------------------------------------------
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
